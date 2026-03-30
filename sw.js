1a16f4f4782bbd3d283711459ba376f6685aa936
chocomoruch <hyk350726@gmail.com>
2026-03-30 15:55:15 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'coc-editor-v1';
+const ASSETS = [
+  './',
+  './CharacterSheet.html',
+  './manifest.json'
+];
+
+// インストール時にファイルをキャッシュ
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// オフライン時はキャッシュから読み込む
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
